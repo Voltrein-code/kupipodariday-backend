f2be47b04d86dfd12b9ea760083f5a0ff7c508c0
Eugene Shalnev <mr-voltrein@yandex.ru>
2025-04-13 21:04:24 +0300

    WIP: Промежуточное сохранение. Разработка схем к БД

--- src/users/entities/user.entity.ts
+++ src/users/entities/user.entity.ts
@@ -1,1 +1,44 @@
-export class User {}
+import { Exclude } from 'class-transformer';
+import { IsEmail, IsString, IsUrl, Length } from 'class-validator';
+import {
+  Column,
+  CreateDateColumn,
+  Entity,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+} from 'typeorm';
+
+@Entity()
+export class User {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @CreateDateColumn()
+  createdAt: Date;
+
+  @UpdateDateColumn()
+  updatedAt: Date;
+
+  @Column({ unique: true })
+  @IsString()
+  @Length(2, 30)
+  username: string;
+
+  @Column({ default: 'Пока ничего не рассказал о себе' })
+  @IsString()
+  @Length(2, 200)
+  about: string;
+
+  @Column({ default: 'https://i.pravatar.cc/300' })
+  @IsUrl()
+  avatar: string;
+
+  @Column({ unique: true, select: false })
+  @IsEmail()
+  email: string;
+
+  @Column({ select: false })
+  @IsString()
+  @Exclude()
+  password: string;
+}
--- src/wishes/entities/wish.entity.ts
+++ src/wishes/entities/wish.entity.ts
@@ -1,1 +1,41 @@
-export class Wish {}
+import { IsPositive, IsString, IsUrl, Length } from 'class-validator';
+import {
+  Column,
+  CreateDateColumn,
+  Entity,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+} from 'typeorm';
+
+@Entity()
+export class Wish {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @CreateDateColumn()
+  createdAt: Date;
+
+  @UpdateDateColumn()
+  updatedAt: Date;
+
+  @Column()
+  @IsString()
+  @Length(1, 250)
+  name: string;
+
+  @Column()
+  @IsUrl()
+  link: string;
+
+  @Column()
+  @IsUrl()
+  image: string;
+
+  @Column()
+  @IsPositive()
+  price: number;
+
+  @Column()
+  @IsPositive()
+  raised: number;
+}
