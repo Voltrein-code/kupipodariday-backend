d02cb96c2060cb25953f38756a479c5aac0ad252
voltrein <mr-voltrein@yandex.ru>
2025-04-14 11:16:54 +0300

    feat: Добавить импорт модулей

--- src/wishes/wishes.module.ts
+++ src/wishes/wishes.module.ts
@@ -1,9 +1,15 @@
 import { Module } from '@nestjs/common';
 import { WishesService } from './wishes.service';
 import { WishesController } from './wishes.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Wish } from './entities/wish.entity';
+import { User } from 'src/users/entities/user.entity';
+import { Offer } from 'src/offers/entities/offer.entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Wish, User, Offer])],
   controllers: [WishesController],
   providers: [WishesService],
+  exports: [WishesService],
 })
 export class WishesModule {}
